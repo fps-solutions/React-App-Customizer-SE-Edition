40def2533f126424fed8d1777294e6a350416621
mikezimm <49648086+mikezimm@users.noreply.github.com>
2024-08-02 01:02:39 -0400

    Add react dependencies

---
+++ src/extensions/reactAppCustomizerSe/components/ReactFooter.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";  
+import { Link } from 'office-ui-fabric-react/lib/Link';  
+import { CommandBar } from 'office-ui-fabric-react/lib/CommandBar';  
+  
+export interface IReactFooterProps {}  
+  
+export default class ReactFooter extends React.Component<IReactFooterProps> {  
+  constructor(props: IReactFooterProps) {  
+    super(props);  
+  }  
+  
+  public render(): JSX.Element {  
+    return (  
+      <div className={"ms-bgColor-themeDark ms-fontColor-white"}>  
+        <CommandBar  
+          items={this.getItems()}  
+        />       
+      </div>  
+    );  
+  }  
+  
+  // Data for CommandBar  
+  private getItems = () => {  
+    return [  
+      {  
+        key: 'microsoft',  
+        name: 'Microsoft',  
+        cacheKey: 'myCacheKey', // changing this key will invalidate this items cache  
+        iconProps: {  
+          iconName: 'AzureLogo'  
+        },  
+        href: 'https://www.Microsoft.com'  
+      },  
+      {  
+        key: 'officeUIFabric',  
+        name: 'Office UI Fabric',  
+        iconProps: {  
+          iconName: 'OneDrive'  
+        },  
+        href: 'https://dev.office.com/fabric',  
+        ['data-automation-id']: 'uploadButton'  
+      }  
+    ];  
+  }  
+}
